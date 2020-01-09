128002e664e4aa42461d94bf21ba87af43dd61ac
realsep <58202232+realsep@users.noreply.github.com>
2020-01-09 12:18:19 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -8,21 +8,21 @@ function initializeApp(data) {
     document.getElementById('sendmessagebutton').addEventListener('click', function() {
         var msg = document.getElementById('bchcode').value;
         // /////////////////////////////////////////////////
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    alert(this.responseText);
-                }
-            };
-            // var x = 
-            xhttp.open("POST", "https://stormy-spire-09445.herokuapp.com/liff", true);
-            xhttp.setRequestHeader("Content-Type", "application/json");
-            xhttp.send("test")
+//             var xhttp = new XMLHttpRequest();
+//             xhttp.onreadystatechange = function() {
+//                 if (this.readyState == 4 && this.status == 200) {
+//                     alert(this.responseText);
+//                 }
+//             };
+//             // var x = 
+//             xhttp.open("POST", "https://stormy-spire-09445.herokuapp.com/liff", true);
+//             xhttp.setRequestHeader("Content-Type", "application/json");
+//             xhttp.send("test")
         
 
         liff.sendMessages([{
             type: 'text',
-            text: xhttp
+            text: msg
         }]).then(function() {
             liff.closeWindow();
         }).catch(function(error) {

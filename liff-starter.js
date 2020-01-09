5909b5aa6273658e26c471be540fa4ff5a66541d
realsep <58202232+realsep@users.noreply.github.com>
2020-01-09 10:14:07 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -7,19 +7,14 @@ window.onload = function(e) {
 function initializeApp(data) {
     document.getElementById('sendmessagebutton').addEventListener('click', function() {
         var msg = document.getElementById('bchcode').value;
-        var msg1 = document.getElementById('date_start').value;
-        var msg2 = document.getElementById('date_stop').value;
-
+       
 
 
 
         liff.sendMessages([{
             type: 'text',
             text: msg,
-            type: 'text',
-            text: 'msg1',
-            type: 'text',
-            text: 'msg2'
+            
         }]).then(function() {
             liff.closeWindow();
         }).catch(function(error) {

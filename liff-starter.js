407aed25f389e55ec85bc6b118a72e3b216d9b5e
realsep <58202232+realsep@users.noreply.github.com>
2020-01-10 13:32:30 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -9,26 +9,33 @@ function initializeApp(data) {
         var x;
         var msg = document.getElementById('bchcode').value;
 
-        function response(res) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     x = this.responseText;
-                }
-            };
-            xhttp.open("POST", "https://stormy-spire-09445.herokuapp.com/liff", true);
-            xhttp.setRequestHeader("Content-Type", "application/json");
-            xhttp.send();
+                    liff.sendMessages([{
+                        type: 'text',
+                        text: x
+                    }]).then(function() {
+                        liff.closeWindow();
+                    }).catch(function(error) {
+                        window.alert("Error sending message: " + error);
+                    });
 
-        }
-        liff.sendMessages([{
-            type: 'text',
-            text: x
-        }]).then(function() {
-            liff.closeWindow();
-        }).catch(function(error) {
-            window.alert("Error sending message: " + error);
-        });
+                };
+                xhttp.open("POST", "https://stormy-spire-09445.herokuapp.com/liff", true);
+                xhttp.setRequestHeader("Content-Type", "application/json");
+                xhttp.send();
+
+            }
+            // liff.sendMessages([{
+            //     type: 'text',
+            //     text: x
+            // }]).then(function() {
+            //     liff.closeWindow();
+            // }).catch(function(error) {
+            //     window.alert("Error sending message: " + error);
+            // });
     });
 
 }

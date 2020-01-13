69ac998c7456a74f1ab62cd8a1a3b5a638c968c5
realsep <58202232+realsep@users.noreply.github.com>
2020-01-13 11:10:58 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,30 +1,41 @@
 window.onload = function(e) {
-    // liff.init(function(data) {
-    initializeApp();
-    // });
+    liff.init(function(data) {
+        initializeApp();
+    });
 };
 
 function initializeApp() {
     document.getElementById('sendmessagebutton').addEventListener('click', function() {
-        var x = ('2');
+        var x;
         var msg = document.getElementById('bchcode').value;
 
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    x = this.responseText;            
+                    x = this.responseText;
+                    liff.sendMessages([{
+                        type: 'text',
+                        text: x
+                    }]).then(function() {
+                        liff.closeWindow();
+                    }).catch(function(error) {
+                        window.alert("Error sending message: " + error);
+                    });
+
                 };
                 xhttp.open("POST", "https://stormy-spire-09445.herokuapp.com/liff", true);
                 xhttp.setRequestHeader("Content-Type", "application/json");
                 xhttp.send();
 
             }
-            liff.sendMessages([{
-                type: 'text',
-                text: x
-            }]).then(function() {
-                liff.closeWindow();
-            }).catch(function(error) {
-                window.alert("Error sending message: " + error);
-            });
+            // liff.sendMessages([{
+            //     type: 'text',
+            //     text: x
+            // }]).then(function() {
+            //     liff.closeWindow();
+            // }).catch(function(error) {
+            //     window.alert("Error sending message: " + error);
+            // });
     });
+
+}

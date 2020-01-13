a61a8ee31127ea41a0a58aa8a28c443ec1a4590b
realsep <58202232+realsep@users.noreply.github.com>
2020-01-13 09:27:45 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -13,14 +13,7 @@ function initializeApp() {
         xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     x = this.responseText;
-                    liff.sendMessages([{
-                        type: 'text',
-                        text: x
-                    }]).then(function() {
-                        liff.closeWindow();
-                    }).catch(function(error) {
-                        window.alert("Error sending message: " + error);
-                    });
+                    
 
                 };
                 xhttp.open("POST", "https://stormy-spire-09445.herokuapp.com/liff", true);
@@ -28,14 +21,14 @@ function initializeApp() {
                 xhttp.send();
 
             }
-            // liff.sendMessages([{
-            //     type: 'text',
-            //     text: x
-            // }]).then(function() {
-            //     liff.closeWindow();
-            // }).catch(function(error) {
-            //     window.alert("Error sending message: " + error);
-            // });
+            liff.sendMessages([{
+                type: 'text',
+                text: x
+            }]).then(function() {
+                liff.closeWindow();
+            }).catch(function(error) {
+                window.alert("Error sending message: " + error);
+            });
     });
 
 }
